db103d8db7aa7232793460191a83999d2649b975
Mike <shpoopdy@gmail.com>
2023-04-16 08:52:26 -0700

    Add quotes

--- quote.js
+++ quote.js
@@ -1,3 +1,17 @@
 const quotes = [
-"The appearance of the island when I came on deck next morning was altogether changed. Although the breeze had now utterly ceased, we had made a great deal of way during the night and were now lying becalmed about half a mile to the south-east of the low eastern coast. -Robert Louis Stevenson"
+"The appearance of the island when I came on deck next morning was altogether changed. Although the breeze had now utterly ceased, we had made a great deal of way during the night and were now lying becalmed about half a mile to the south-east of the low eastern coast. -Robert Louis Stevenson",
+"Many of life's failures are people who did not realize how close they were to success when they gave up. -Thomas A. Edison",
+"Keep smiling, because life is a beautiful thing and there's so much to smile about. -Marilyn Monroe",
+"You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose. -Dr. Seuss",
+"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma — which is living with the results of other people's thinking. -Steve Jobs",
+"The real test is not whether you avoid this failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere. -Barack Obama",
+"You know you are on the road to success if you would do your job and not be paid for it. -Oprah Winfrey",
+"I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel. -Maya Angelou",
+"I alone cannot change the world, but I can cast a stone across the water to create many ripples. -Mother Teresa",
+"How wonderful it is that nobody need wait a single moment before starting to improve the world. -Anne Frank",
+"Nothing is impossible, the word itself says, 'I'm possible!' -Audrey Hepburn",
+"You only live once, but if you do it right, once is enough. ―Mae West",
+"Life imposes things on you that you can't control, but you still have the choice of how you're going to live through this. —Celine Dion",
+"Life is ten percent what happens to you and ninety percent how you respond to it. —Charles Swindoll",
+"Life would be tragic if it weren't funny. — Stephen Hawking"
 ]
